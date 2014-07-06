575aa756f529806b449a9c3d5824d8e29fd3c8a9
Daniel Alejandro Cast <dan_lex_castnirvana@hotmail.com>
2014-07-05 22:15:51 -0600

    Menu for backend created.

--- src/ItesAC/BackendBundle/Menu/Builder.php
+++ src/ItesAC/BackendBundle/Menu/Builder.php
@@ -13,9 +13,12 @@ class Builder {
 
     public function menuAction(FactoryInterface $factory, array $options) {
         $menu = $factory->createItem('root');
-
-//        $menu->addChild('Control', array('route' => ''));
-        //se agregara uno a control y los demas de cada elemento del backend
+        
+        $menu->addChild('Aire Acondicionado', array('route' => 'ac'));
+        $menu->addChild('Planta', array('route' => 'planta'));
+        $menu->addChild('Edificio', array('route' => 'edificio'));
+        $menu->addChild('Arduino', array('route' => 'arduino'));
+        $menu->addChild('Modelo', array('route' => 'modelo'));
         
 
         return $menu;
